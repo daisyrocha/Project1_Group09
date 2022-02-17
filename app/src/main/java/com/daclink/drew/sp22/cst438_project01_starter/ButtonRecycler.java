ef5483f504def7b73845bf9d7de87560bdcddf98
Daisy Rocha-Montanez <72002539+daisyrocha@users.noreply.github.com>
2022-02-16 20:19:42 -0800

    Add files via upload
    This works and will display our Data that we pulled from the API in our RecyclerView

--- app/src/main/java/com/daclink/drew/sp22/cst438_project01_starter/ButtonRecycler.java
+++ app/src/main/java/com/daclink/drew/sp22/cst438_project01_starter/ButtonRecycler.java
@@ -13,10 +13,6 @@ import android.widget.Toast;
 
 import com.example.fitnessapp.databinding.ActivityButtonRecyclerBinding;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -27,7 +23,7 @@ public class ButtonRecycler extends AppCompatActivity {
     private ActivityButtonRecyclerBinding binding;
     private FitnessAppDatabase db;
 
-    RecyclerView recyclerView;
+    RecyclerView recyclerView;  //activity
     ProgressBar progressBar;
     LinearLayoutManager layoutManager;
     ButtonAdapter adapter;
@@ -46,12 +42,12 @@ public class ButtonRecycler extends AppCompatActivity {
          * We create an instance of recycler view and pass the list we created
          * We then create this same constructor in the button adapter
          */
-        recyclerView = binding.reclerview;
+        recyclerView = binding.recycView;
         progressBar = binding.progressBar;
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
-        adapter = new ButtonAdapter(optionsList);
-        recyclerView.setAdapter(adapter);
+//        adapter = new ButtonAdapter(optionsList);
+//        recyclerView.setAdapter(adapter);
 
         fetchData();
 
@@ -65,6 +61,9 @@ public class ButtonRecycler extends AppCompatActivity {
             public void onResponse(Call<ExerciseInfoJson<MuscleCategory>> call, Response<ExerciseInfoJson<MuscleCategory>> response) {
                 if(response.isSuccessful() && response.body() != null) {
                     optionsList = (response.body());
+                    //inserted
+                    adapter = new ButtonAdapter(optionsList);
+                    recyclerView.setAdapter(adapter);
                     Log.d("GettingData", optionsList.toString());
                     adapter.notifyDataSetChanged();
                     progressBar.setVisibility(View.GONE);
